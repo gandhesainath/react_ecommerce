12afbe5138b1a6ab22c08b937968390a558ca080
gandhesainath <sainathgandhe200324@gmail.com>
2025-05-23 01:14:26 +0530

    images working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import "./styles.css";
+import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Header-Footer/Navbar";
 import Register from "./Components/Auth/Register";
